67be7ebdbb201436dcd0712810968b8909872770
firas hamila <firas.hm.1996@gmail.com>
2025-04-08 16:02:10 +0200

    a

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
@@ -10,6 +16,9 @@ export default function App() {
           placeholder="useless placeholder"
           keyboardType="numeric"
         />
+        <TouchableOpacity style={styles.btn}>
+          <Text style={styles.btnTXT}>Add</Text>
+        </TouchableOpacity>
       </View>
       <View style={styles.v2}></View>
     </View>
@@ -24,14 +33,33 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     marginBottom: 15,
   },
-  v1: { width: "100%", flex: 1, marginTop: 48 },
-  v2: { width: "100%", flex: 8 },
+  v1: {
+    width: "100%",
+    flex: 1,
+    marginTop: 48,
+    backgroundColor: "red",
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+  },
+  v2: { width: "100%", flex: 8, backgroundColor: "blue" },
   input: {
     height: 40,
-    margin: 12,
     borderWidth: 1,
     padding: 10,
     borderRadius: 12,
     borderColor: "#ccc",
+    width: "70%",
+  },
+  btnTXT: {
+    color: "white",
+    fontSize: 22,
+    fontWeight: "bold",
+  },
+  btn: {
+    backgroundColor: "green",
+    paddingVertical: 8,
+    paddingHorizontal: 12,
+    borderRadius: 12,
   },
 });
---
+++ src/components/TestItem.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const TestItem = () => {
+  return (
+    <View>
+      <Text>TestItem</Text>
+    </View>
+  );
+};
+
+export default TestItem;
+
+const styles = StyleSheet.create({});
